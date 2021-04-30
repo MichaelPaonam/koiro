3676443bc742e5ce0856bd724eebfdac4a5a62e7
Michael Paonam <mazdaswift@gmail.com>
2021-04-30 21:47:16 +0530

    made changes to background

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,13 @@ import './HeroSection.css';
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
+    <div className='hero-container' style={{
+      backgroundImage: "url('/images/img-camping.jpg')",
+    backgroundPosition: 'center',
+    backgroundSize: 'center/cover',
+    backgroundRepeat: 'no-repeat',
+
+    }}>
       {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
