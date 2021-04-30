1ef87fe59a0f0801b10ffe1fbf258ac022b24d51
Michael Paonam <mazdaswift@gmail.com>
2021-04-30 18:12:54 +0530

    Updates

---
+++ precache-manifest.5a9005b601cc852fc6985ec620b6159a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d007d7ccebc233b220fdc0847be9bc20",
+    "url": "/koiro/index.html"
+  },
+  {
+    "revision": "d258a285f7a0326debcd",
+    "url": "/koiro/static/css/main.ba4d057c.chunk.css"
+  },
+  {
+    "revision": "50e7435fbbc4d71cffda",
+    "url": "/koiro/static/js/2.3d573bab.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/koiro/static/js/2.3d573bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d258a285f7a0326debcd",
+    "url": "/koiro/static/js/main.5784a590.chunk.js"
+  },
+  {
+    "revision": "7d457535de0044db0d60",
+    "url": "/koiro/static/js/runtime-main.31b68538.js"
+  }
+]);
