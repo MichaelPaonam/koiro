79a2aeb34823322b12bd36b882a75f6becb4cd8d
Michael Paonam <mazdaswift@gmail.com>
2021-04-30 22:11:29 +0530

    using vimeo link

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,8 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container' >
-      <video src='videos/video-1.mp4' autoPlay loop muted />
+      {/* <video src='videos/video-1.mp4' autoPlay loop muted /> */}
+      <video src="https://player.vimeo.com/external/342571552.hd.mp4?s=6aa6f164de3812abadff3dde86d19f7a074a8a66&profile_id=175&oauth2_token_id=57447761" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
