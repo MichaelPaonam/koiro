b833ed9399a89439ea15e084052e822bc609f8fc
Michael Paonam <mazdaswift@gmail.com>
2021-04-30 20:55:36 +0530

    added gitignore

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 // import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
